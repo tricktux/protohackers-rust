1468072aa94a5cc3d687296655022e72608ac824
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2026-02-13 06:24:56 -0500

    fix: baseline for a simple echo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 // Looking at: https://doc.rust-lang.org/std/net/struct.TcpListener.html
+use std::io::{self, Read, Write};
 use std::net::{TcpListener, TcpStream};
 
-fn handle_client(stream: TcpStream) {
-    // ...
+fn handle_client(mut stream: TcpStream) {
+    let mut data = vec![0u8; 256];
+    loop {
+        match stream.read(&mut data) {
+            Ok(0) => break, // Connection closed
+            Ok(_) => {
+                // Write the data back
+                // Implement echo
+                match stream.write_all(&data) {
+                    Ok(_) => continue, // Successfully echoed back, wait for more data
+                    Err(e) => {
+                        println!("Error writing to stream: {}", e);
+                        break; // Exit on write error
+                    }
+                }
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue, // No data available, try again
+            Err(e) => panic!("IO error on stream.read: {}", e),              // Handle other errors
+        }
+    }
 }
 
 fn main() -> std::io::Result<()> {
@@ -10,7 +29,15 @@ fn main() -> std::io::Result<()> {
 
     // accept connections and process them serially
     for stream in listener.incoming() {
-        handle_client(stream?);
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr()?);
+                handle_client(stream);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
     }
     Ok(())
 }
