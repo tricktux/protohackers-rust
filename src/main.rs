eba6f356064587400034e1fb33b4d3e029449b52
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2026-02-13 07:00:34 -0500

    fix: avoid nested match
    - Also fix the port number
    - Introduce `?` when it doesn't matter

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,39 @@
 // Looking at: https://doc.rust-lang.org/std/net/struct.TcpListener.html
-use std::io::{self, Read, Write};
+use std::io;
+use std::io::{Read, Result, Write};
 use std::net::{TcpListener, TcpStream};
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream) -> Result<()> {
     let mut data = vec![0u8; 256];
     loop {
-        match stream.read(&mut data) {
-            Ok(0) => break, // Connection closed
-            Ok(_) => {
-                // Write the data back
-                // Implement echo
-                match stream.write_all(&data) {
-                    Ok(_) => continue, // Successfully echoed back, wait for more data
-                    Err(e) => {
-                        println!("Error writing to stream: {}", e);
-                        break; // Exit on write error
-                    }
-                }
+        let n = match stream.read(&mut data) {
+            Ok(0) => {
+                print!("Gracefully closing the connection");
+                break;
             }
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue, // No data available, try again
-            Err(e) => panic!("IO error on stream.read: {}", e),              // Handle other errors
-        }
+            Ok(n) => n,
+            Err(e) if e.kind() == io::ErrorKind::TimedOut => {
+                println!("Client idle, closing connection");
+                break;
+            }
+            Err(e) => return Err(e), // Propagate other errors (like ?)
+        };
+
+        // Write - simple ? since we don't care about specific errors
+        stream.write_all(&data[..n])?;
     }
+    Ok(())
 }
 
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:80")?;
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:18888")?;
 
     // accept connections and process them serially
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 println!("New connection: {}", stream.peer_addr()?);
-                handle_client(stream);
+                handle_client(stream)?;
             }
             Err(e) => {
                 println!("Error: {}", e);
